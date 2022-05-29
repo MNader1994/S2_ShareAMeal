d30c12ddd0f8b4c96dd63cde6114c0aa8c5f4711
Birakdar,Nader M.N <nader.birakdar@student.fontys.nl>
2022-05-29 18:21:11 +0200

    Port Heroku Fixed!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const { resourceUsage } = require('process')
 
-const port = 3000
+const port = process.env.PORT || 3000
 const result = {
     code: 200,
     message:'Step on the moon'
