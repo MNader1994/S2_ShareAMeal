b7e9425f4bae4660a99d2772c7d08e59800c5ac3
Birakdar,Nader M.N <nader.birakdar@student.fontys.nl>
2022-06-04 14:12:50 +0200

    adding basic routing

--- index.js
+++ index.js
@@ -27,6 +27,56 @@ app.get("/", (req, res) => {
 
 
 
+app.post("/api/user", (req, res) =>{
+  let user = req.body;
+  id++;
+
+  user = {
+    id,
+    ...user,
+  };
+  console.log(user);
+  database.push(user);
+  res.status(201).json({
+    status: 201,
+    result: database,
+  });
+}); // UC-201 register a new user 
+
+app.get("/api/user", (req, res) => {
+  res.status(200).json({
+    status:200,
+    result: database,
+  });
+}) // UC-202 get all users 
+
+
+app.get("/api/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  console.log(`User met Id ${userId} gezocht`);
+  let user = database.filter((item) => item.id == userId);
+  if (user.length > 0) {
+    console.log(user);
+    res.status(200).json({
+      status: 200,
+      result: user,
+    });
+  }
+  else {
+    res.status(401).json({
+      status: 401,
+      result: `User with ID ${userId} not found`,
+    });
+  }
+}); // UC-204 get a specific user using their Id
+
+
+app.all("*", (req,res) => {
+  res.status(401).json({
+    status:401,
+    result: "End-point not found",
+  });
+});
 
 
 app.listen(port, () => {
