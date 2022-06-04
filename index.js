3d9e38e506fbe4815fbff776c58edf400a2c0127
Birakdar,Nader M.N <nader.birakdar@student.fontys.nl>
2022-06-04 13:40:58 +0200

    adding feature-express without basic routing

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const http = require('http')
+const express = require('express');
+const app = express();
 
 const port = process.env.PORT || 3000
-const result = {
-    code: 200,
-    message:'Step on the moon'
-}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end(JSON.stringify(result))
-})
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-server.listen(port, () => {
+
+
+let database = [];
+let id =0;
+
+app.all("*", (req, res, next) => {
+  const method = req.method;
+  console.log(`Methode ${method} is aangeroepen`);
+  next();
+}); // getting the name of any requsted method 
+
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: "Step on the moon"
+  });
+}); // returns step on the moon as first result on requsting localhost
+
+
+
+
+
+app.listen(port, () => {
   console.log(`Server running at ${port}`)
 })
